83ec0a815dfda01a7aae0c044af01c5af2fb463b
Mohamed <Mohamed.G.Ali@seattlecolleges.edu>
2020-07-12 23:17:10 -0700

    second upload

---
+++ about.js
@@ -0,0 +1,9 @@
+
+<html>
+    <body>
+        <h1>About Me</h1>
+        <p>My name is MOE and I study programming</p>
+
+        <script>console.log('about me')</script>
+    </body>
+</html>
---
+++ index.js
@@ -0,0 +1,37 @@
+
+'use strict'
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
+
+// send static file as response
+app.get('/', (req, res) => {
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/home.html'); 
+ });
+ 
+ // send plain text response
+ app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('About page');
+ });
+
+ // define 404 handler
+app.use( (req,res) => {
+  res.type('text/plain'); 
+  res.status(404);
+  res.send('404 - Not found');
+ });
+ app.listen(app.get('port'), () => {
+  console.log('Express started'); 
+ });
+
+ let exphbs = require("express-handlebars"); // should be at top of module 
+
+app.engine('handlebars', exphbs({defaultLayout: false}));
+app.set("view engine", "handlebars");
